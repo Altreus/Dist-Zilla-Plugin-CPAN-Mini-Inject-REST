0c88f3929f583ded4ae7fce7e5f07c4dc46a3fec
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2015-07-24 18:02:07 +0100

    Data::Dump the result if it's still a hashref

--- lib/Dist/Zilla/Plugin/CPAN/Mini/Inject/REST.pm
+++ lib/Dist/Zilla/Plugin/CPAN/Mini/Inject/REST.pm
@@ -5,6 +5,7 @@ package Dist::Zilla::Plugin::CPAN::Mini::Inject::REST;
 our $VERSION = '0.001';
 
 use Moose;
+use Data::Dump;
 extends 'CPAN::Mini::Inject::REST::Client::API';
 
 with 'Dist::Zilla::Role::Releaser';
@@ -15,11 +16,31 @@ has '+protocol' => ( default => 'http' );
 sub release {
     my ($self, $archive) = @_;
 
+    $self->log_debug(sprintf "Sending %s to %s://%s:%d", $archive, $self->protocol, $self->host, $self->port);
+
     my ($code, $result) = $self->post(
         qq(repository/$archive) => {
-            file => [$archive]
+            file => ["$archive"]
         }
     );
+    
+    if ($code >= 500) {
+        # FIXME - does it give us info?
+        $self->log_fatal("$code: Server error");
+    }
+    elsif ($code >= 400) {
+        my $error = ref $result ? $result->{error} : $result;
+
+        $error = pp $error if ref $error;
+
+        $self->log_fatal("$code: Client error: $error");
+    }
+    elsif ($code >= 300) {
+        $self->log_fatal("Unexpected $code that wasn't followed");
+    }
+    else {
+        $self->log("Successfully indexed $archive");
+    }
 }
 
 1;
